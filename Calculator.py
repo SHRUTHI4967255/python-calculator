a4d4985996c6a111f033b4c74829db823c28dbf6
SHRUTHI4967255 <s4967255@gmail.com>
2026-06-20 14:15:12 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,26 @@
+print("HI")
+first = input("First: ")
+second = input("Second: ")
+sum = first + second
+print(sum)
+
+first = input("First: ")
+second = input("second: ")
+sum = int(first) + int(second)
+print(sum)
+
+print("Age calculation")
+birth_year = input("Enter your birth year: ")
+age = 2026 - int(birth_year)
+print(age)
+
+first = input("First: ")
+second = input("Second: ")
+Difference = int(first) - int(second)
+print(Difference)
+
+first = input("First: ")
+second = input("Second: ")
+Devision = int(first) / int(second)
+print(Devision)
+
