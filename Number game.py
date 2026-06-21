b8760402eacb0d599fb6b0ffc9717129e90a7f70
K Shruthi <s4967255@gmail.com>
2026-06-21 16:03:36 +0530

    Number guessing game
    Actually this 2nd project. This is a small number guessing game, which was done by my own.
    this game has 7 questions, the game can say that whether the given answer is correct or wrong.
    its just a fun game.

---
+++ Number game.py
@@ -0,0 +1,44 @@
+x = 123
+guess = int(input("guess x:"))
+if guess == x:
+    print("correct")
+else:
+     print("wrong")
+
+y = 654
+guess = int(input("guess y:"))
+if guess == y:
+         print("correct")
+else:
+           print("wrong")
+
+f = 77
+guess = int(input("guess f:"))
+if guess == f:
+         print("correct")
+else:
+           print("wrong")
+d = 85
+guess = int(input("guess d:"))
+if guess == d:
+         print("correct")
+else:
+           print("wrong")
+c = 58
+guess = int(input("guess c:"))
+if guess == c:
+         print("correct")
+else:
+           print("wrong")
+b = 46
+guess = int(input("guess b:"))
+if guess == b:
+         print("correct")
+else:
+           print("wrong")
+a = 64
+guess = int(input("guess a:"))
+if guess == a:
+         print("correct")
+else:
+           print("wrong")
