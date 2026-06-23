bd15d8b448fa03cbc16b66167835e426af8acbe1
K Shruthi <s4967255@gmail.com>
2026-06-23 13:31:42 +0530

    FIFA world cup details
    This project contains details like Date, Venu(place where the match held), Winner team, Runner up teams.
    This is my best project Till now. As just a beginner doing this much big code was a big thing to me.
    In this project, as the user enters the year at which they want the details about the match, program will show the month, date, venu, won team, lost team.
    This is a project about FIFA WORLDCUP.
    When I started this project then the 2026 FIFA matches were going on, so I didn't enter that year.
    This project have details of 22 matches from 1930 to 2002

---
+++ FIFA worldcup detials.py
@@ -0,0 +1,115 @@
+print("FIFA WORLD CUP DETAILS")
+year = int(input("1930 or 1934 or 1938 or 1950 or 1954 or 1958 or 1962 or 1966 or 1970 or 1978 or 1982 or 1986 or 1990 or 1994 or 1998 or 2002 or 2006 or 2010 or 2014 or 2018 or 2022: "))
+
+if year == 1930:
+    print("date - 30 july")
+    print("place - uruguay")
+    print("Winner - Uruguay")
+    print("Runner up - Argentina")
+elif year == 1934:
+    print("date - 10 june")
+    print("place - Italy")
+    print("Winner - Italy")
+    print("Runner up - Czechoslovakia")
+elif year == 1938:
+    print("date - 19 june")
+    print("place - France")
+    print("Winner - Italy")
+    print("Runner up - Hungary")
+elif year == 1950:
+    print("date - 16 july")
+    print("place - Brazil")
+    print("Winner - Uruguay")
+    print("Runner up - Brail")
+elif year == 1954:
+    print("date - 4 july")
+    print("place - Switzerland")
+    print("Winner - Germany")
+    print("Runner up - Hungary")
+elif year == 1958:
+    print("date - 29 june")
+    print("place - Sweden")
+    print("Winner - Brazil")
+    print("Runner up - Sweden")
+elif year == 1962:
+    print("date - 17 june")
+    print("place - Satiago")
+    print("Winner - Brazil")
+    print("Runner up - Czechoslovakia")
+elif year == 1966:
+    print("date - 30 july")
+    print("place - England")
+    print("Winner - England")
+    print("Runner up - Germany")
+elif year == 1970:
+    print("date - 21 june")
+    print("place - Mexico")
+    print("Winner - Brazil")
+    print("Runner up - Italy")
+elif year == 1974:
+    print("date - 7 july")
+    print("place - Germany")
+    print("Winner - Germany")
+    print("Runner up - Netherlands")
+elif year == 1978:
+    print("date - 25 june")
+    print("place - Argentina")
+    print("Winner - Argentina")
+    print("Runner up - Netherlands")
+elif year == 1982:
+    print("date - 11 july")
+    print("place - Spain")
+    print("Winner - Italy")
+    print("Runner up - Germany")
+elif year == 1986:
+    print("date - 29 june")
+    print("place - Mexico")
+    print("Winner - Argentina")
+    print("Runner up - Germany")
+elif year == 1990:
+    print("date - 8 july")
+    print("place - Italy")
+    print("Winner - Germany")
+    print("Runner up - Argentina")
+elif year == 1994:
+    print("date - 17 july")
+    print("place - United states")
+    print("Winner - Brazil")
+    print("Runner up - Italy")
+elif year == 1998:
+    print("date - 12 july")
+    print("place - France")
+    print("Winner - France")
+    print("Runner up - Brazil")
+elif year == 2002:
+    print("date - 30 june")
+    print("place - Japan")
+    print("Winner - Brazil")
+    print("Runner up - Germany")
+elif year == 2006:
+    print("date - 9 july")
+    print("place - Germany")
+    print("Winner - Italy")
+    print("Runner up - France")
+elif year == 2010:
+    print("date - 11 july")
+    print("place - South africa")
+    print("Winner - Spain")
+    print("Runner up - Netherlands")
+elif year == 2014:
+    print("date - 13 july")
+    print("place - Brazil")
+    print("Winner - Germany")
+    print("Runner up - Argentina")
+elif year == 2018:
+    print("date - 15 july")
+    print("place - Russia")
+    print("Winner - France")
+    print("Runner up - Croatia")
+elif year == 2022:
+    print("date - 18 dec")
+    print("place - Qatar")
+    print("Winner - Argentina")
+    print("Runner up - France")
+
+input("Press Enter to exit.......")
