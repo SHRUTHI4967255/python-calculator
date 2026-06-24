6fdd8dd1c7af314a1b3f7fc8355491d3cc5bf768
K Shruthi <s4967255@gmail.com>
2026-06-25 00:56:34 +0530

    Planets and their 3 facts
    This project is about the planets and their 3 facts including the number of moons. User can enter planet name, they can know the facts and the number of moons associated with that planet.
    This is one of my projects done when I was learning Python Fundamentals.

---
+++ Planets and their facts.py
@@ -0,0 +1,39 @@
+print("PLANETS")
+print("Their are 8 planets")
+print(" 4 are inner planets and 4 are outer planets ")
+planet = input("Enter the planet name: ")
+planet = planet.capitalize()
+if planet == "Mercury":
+    print('Smallest and closest planet to sun')
+    print('88 Earth days for 1 revolution')
+    print("0 moons")
+elif planet == "Venus":
+    print("Earth's twin in size but different in environment")
+    print("Hottest planet in the solar system")
+    print("0 moons")
+elif planet == "Earth":
+    print("Our well know favorite planet")
+    print("71% of water and Billions of humans")
+    print("1 moon")
+elif planet == "Mars":
+    print("Know as red planet")
+    print("Has the largest volcano in the solar system")
+    print(" 2 moons")
+elif planet == "Jupiter":
+    print("Largest planet in the solar system")
+    print("Famous for 'Great red spot'")
+    print("95 moons")
+elif planet == "Saturn":
+    print("Second largest planet")
+    print("Famous for 'ice and dust ring'")
+    print("274 moons")
+elif planet == "Uranus":
+    print("Blue green 3rd largest planet in solar system")
+    print("It has 21 years long winter")
+    print("28 moons")
+elif planet == "Neptune":
+    print("Farest planet from sun")
+    print("Windiest planet is the solar system")
+    print("16 moons")
+
+input("Press enter to exit.....")
