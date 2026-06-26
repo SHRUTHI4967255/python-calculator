421f98522cb1f628074680efaf862476f514938c
K Shruthi <s4967255@gmail.com>
2026-06-26 14:56:19 +0530

    Updated Maths Quiz
    This is a small Math calculation quiz contains 5 tricky questions.
    I used while loops 
    Variables 
    input() 
    int() 
    Arithmetic operators (+, -, *, //, **) 
    Comparison operators (==) 
    if / else 
    
    This is a project done by me while learning python fundamentals. It is one of my beginner Python projects and was built to practice loops, operators, user input, and conditional statements.

---
+++ Maths Quiz.py
@@ -0,0 +1,60 @@
+a = 2
+while a <= 17:
+    print(a)
+    a = a + 3
+
+print("If the difference between the numbers is written as 'x' ")
+answer = int(input("'x' + 6 * 23 // 5 * 10 =  "))
+if answer == 273:
+    print("CORRECT")
+elif answer != 273:
+    print("wrong please try to find the correct answer")
+
+b = 4
+while b <= 36:
+    print(b)
+    b += 4
+
+print("If the difference between numbers is written as 'y' ")
+answer = int(input(" 12 * 7 - 'y' //4 + 35 =  "))
+if answer == 118:
+    print("CORRECT")
+else:
+    print("WRONG PLEASE TRY TO FIND THE CORRECT ANSWER")
+
+c = 45
+while c >= 9:
+    print(c)
+    c -= 4
+print("If the difference between numbers is written as 'z' ")
+answer  = int(input(" 6 + 'z' * 5 // 2 * 9 =  "))
+if answer == 96:
+    print("CORRECT")
+else:
+    print("WRONG PLEASE TRY AGAIN")
+
+d = 100
+while d >= 20:
+    print(d)
+    d -= 5
+print("If the difference between numbers is written as 'p' ")
+answer = int(input(" 5 + 'p' * 10 // 5 =  "))
+if answer == 15:
+    print("CORRECT")
+else:
+    print("WRONG PLEASE TRY AGAIN")
+
+e = 5
+while e <= 35:
+    print(e)
+    e += 5
+print("If the difference between numbers is written as 'q' ")
+answer = int(input(" 2 + 'q' + 3 - 10 // 2 ** 6 =  "))
+if answer == 10:
+    print("CORRECT")
+else:
+    print("WRONG PLEASE TRY AGAIN")
+
+
+
+print("Press enter to exit:  ")
