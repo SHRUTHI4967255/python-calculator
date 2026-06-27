ed92e516350a4ee88dab311263ac4c22c34bc33e
K Shruthi <s4967255@gmail.com>
2026-06-27 14:48:20 +0530

    This is a Family relations game.
    This is a simple Python-based Family Relations Quiz Game. It presents logical family relationship puzzles where the user selects the correct answer from multiple choices. The game tests reasoning skills using real-life relationship scenarios and provides instant feedback for each answer.

---
+++ Family relations game.py
@@ -0,0 +1,63 @@
+print("         ..............FAMILY RELATIONS GAME..........              ")
+print("1.If sare is priya's mother")
+print("now if kunal is sare's brother")
+print("What will the relation be between priya father and kunal?")
+answer = input("""
+A - BROTHER IN LAW
+B - BROTHER
+C - HUSBAND
+D - FATHER
+Enter your answer: """).upper()
+if answer == "A":
+    print("Correct")
+else:
+    print("Wrong answer")
+
+print("2.If siya is masha sister")
+print("If masha's brother is Bora" )
+print("Hera is Bora's wife")
+print("What will the relation be between Siya and Hera?")
+answer = input("""
+A - SISTER IN LAW
+B - MOTHER
+C - SISTER
+D - AUNT
+Enter your answer: """).upper()
+if answer == "A":
+    print("Correct")
+else:
+    print("Wrong answer")
+
+print("3.Rani is harika daughter")
+print("Murali is harika's brother")
+print("Swathi is Murali wife's mother")
+print("What will the relation be between Rani and Swathi?")
+answer = input("""
+A - SISTER IN LAW
+B - GRANDMOTHER
+C - MOTHER
+D - AUNT
+Enter your answer: """).upper()
+if answer == "B":
+    print("Correct")
+else:
+    print("Wrong answer")
+
+print("4.If ravi is siya father")
+print("sudha is ravi mother")
+print("sara is sudha's daughter in law")
+print("What will the relation be between Siya nad sara?")
+answer = input("""
+A - SISTER
+B - GRANDMA
+C - MOTHER
+D - AUNT
+Enter your answer: """).upper()
+if answer == "C":
+    print("Correct")
+else:
+    print("Wrong answer")
+
+
+
+print("PRESS Enter TO EXIT")
