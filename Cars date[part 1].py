8e1fcd59bee3e678bc750ad685eaca9239bf5700
K Shruthi <s4967255@gmail.com>
2026-07-08 13:11:18 +0530

    Data about 45 CAR Brands
    This project has the information about 45 Car brands. With the information of company name,origin country, started year,founders,hadquaters,famous model.
    I used if,elif,else in this project.
    This cars data project has the 2nd part.

---
+++ Cars date[part 1].py
@@ -0,0 +1,322 @@
+print("                  CARS(part-1)                 ")
+print("Here is the data about car companies")
+print("This has company name,origin country,started year,Founders,Headquaters,Famous model")
+company = input("Enter the car company: ")
+if company == "Abarth":
+   print("Italy")
+   print("1949")
+   print("Carlo Abarth")
+   print("Turin, Italy")
+   print("695")
+
+elif company == "Acura":
+   print("Japan")
+   print("1986")
+   print("Honda")
+   print("Torrance, California, USA")
+   print("NSX")
+
+elif company == "Aiways":
+   print("China")
+   print("2017")
+   print("Fu Qiang, Gary Gu")
+   print("Shanghai, China")
+   print("U5")
+
+elif company == "Alfa Romeo":
+   print("Italy")
+   print("1910")
+   print("A.L.F.A. investors")
+   print("Milan, Italy")
+   print("Giulia")
+
+elif company == "Alpine":
+   print("France")
+   print("1955")
+   print("Jean Rédélé")
+   print("Dieppe, France")
+   print("A110")
+
+elif company == "Aston Martin":
+   print("United Kingdom")
+   print("1913")
+   print("Lionel Martin, Robert Bamford")
+   print("Gaydon, England")
+   print("DB5")
+
+elif company == "Audi":
+   print("Germany")
+   print("1909")
+   print("August Horch")
+   print("Ingolstadt, Germany")
+   print("A4")
+
+elif company == "Bentley":
+   print("United Kingdom")
+   print("1919")
+   print("W. O. Bentley")
+   print("Crewe, England")
+   print("Continental GT")
+
+elif company == "BMW":
+   print("Germany")
+   print("1916")
+   print("Karl Rapp, Gustav Otto")
+   print("Munich, Germany")
+   print("3 Series")
+
+elif company == "Bugatti":
+   print("France")
+   print("1909")
+   print("Ettore Bugatti")
+   print("Molsheim, France")
+   print("Chiron")
+
+elif company == "Buick":
+   print("United States")
+   print("1903")
+   print("David Dunbar Buick")
+   print("Detroit, Michigan, USA")
+   print("Enclave")
+
+elif company == "BYD":
+   print("China")
+   print("1995")
+   print("Wang Chuanfu")
+   print("Shenzhen, China")
+   print("Atto 3")
+
+elif company == "Cadillac":
+   print("United States")
+   print("1902")
+   print("Henry M. Leland")
+   print("Detroit, Michigan, USA")
+   print("Escalade")
+
+elif company == "Changan":
+   print("China")
+   print("1862")
+   print("State-owned origins")
+   print("Chongqing, China")
+   print("CS75 Plus")
+
+elif company == "Chery":
+   print("China")
+   print("1997")
+   print("Wuhu Municipal Government")
+   print("Wuhu, China")
+   print("Tiggo 8")
+
+elif company == "Chevrolet":
+   print("United States")
+   print("1911")
+   print("Louis Chevrolet, William C. Durant")
+   print("Detroit, Michigan, USA")
+   print("Silverado")
+
+elif company == "Chrysler":
+   print("United States")
+   print("1925")
+   print("Walter Chrysler")
+   print("Auburn Hills, Michigan, USA")
+   print("300")
+
+elif company == "Citroën":
+   print("France")
+   print("1919")
+   print("André Citroën")
+   print("Poissy, France")
+   print("C3")
+
+elif company == "Cupra":
+   print("Spain")
+   print("2018")
+   print("Created by SEAT")
+   print("Martorell, Spain")
+   print("Formentor")
+
+elif company == "Dacia":
+   print("Romania")
+   print("1966")
+   print("Romanian Government")
+
+elif company == "Daewoo":
+   print("South Korea")
+   print("1967")
+   print("Kim Woo-choong")
+   print("Seoul, South Korea")
+   print("Matiz")
+
+elif company == "Daihatsu":
+   print("Japan")
+   print("1907")
+   print("Founded as Hatsudoki Seizo Co.")
+   print("Ikeda, Osaka, Japan")
+   print("Terios")
+
+elif company == "Dodge":
+   print("United States")
+   print("1900")
+   print("John Dodge & Horace Dodge")
+   print("Auburn Hills, Michigan, USA")
+   print("Charger")
+
+elif company == "Dongfeng":
+   print("China")
+   print("1969")
+   print("Chinese Government")
+   print("Wuhan, Hubei, China")
+   print("Aeolus AX7")
+
+elif company == "Exeed":
+   print("China")
+   print("2017")
+   print("Chery")
+   print("Wuhu, Anhui, China")
+   print("TXL")
+
+elif company == "Faraday Future":
+   print("United States")
+   print("2014")
+   print("Jia Yueting")
+   print("Los Angeles, California, USA")
+   print("FF 91")
+
+elif company == "Ferrari":
+   print("Italy")
+   print("1947")
+   print("Enzo Ferrari")
+   print("Maranello, Italy")
+   print("488 GTB")
+
+elif company == "Fiat":
+   print("Italy")
+   print("1899")
+   print("Giovanni Agnelli and partners")
+   print("Turin, Italy")
+   print("500")
+
+elif company == "Fisker":
+   print("United States")
+   print("2016")
+   print("Henrik Fisker")
+   print("Manhattan Beach, California, USA")
+   print("Ocean")
+
+elif company == "Ford Motor Company":
+   print("United States")
+   print("1903")
+   print("Henry Ford")
+   print("Dearborn, Michigan, USA")
+   print("F-150")
+
+elif company == "Geely":
+   print("China")
+   print("1986")
+   print("Li Shufu")
+   print("Hangzhou, Zhejiang, China")
+   print("Emgrand")
+
+elif company == "Genesis":
+   print("South Korea")
+   print("2015")
+   print("Hyundai Motor Group")
+   print("Seoul, South Korea")
+   print("G90")
+
+elif company == "GMC":
+   print("United States")
+   print("1911")
+   print("General Motors")
+   print("Detroit, Michigan, USA")
+   print("Sierra")
+
+elif company == "Great Wall Motor":
+   print("China")
+   print("1984")
+   print("Wei Jianjun (major builder of the company)")
+   print("Baoding, Hebei, China")
+   print("Haval H6")
+
+elif company == "Haval":
+   print("China")
+   print("2013")
+   print("Great Wall Motor")
+   print("Baoding, Hebei, China")
+   print("H6")
+
+elif company == "Holden":
+   print("Australia")
+   print("1856")
+   print("James Alexander Holden")
+   print("Melbourne, Victoria, Australia")
+   print("Commodore")
+
+elif company == "Honda":
+   print("Japan")
+   print("1948")
+   print("Soichiro Honda & Takeo Fujisawa")
+   print("Tokyo, Japan")
+   print("Civic")
+
+elif company == "Hongqi":
+   print("China")
+   print("1958")
+   print("FAW Group")
+   print("Changchun, Jilin, China")
+   print("H9")
+
+elif company == "Hyundai":
+   print("South Korea")
+   print("1967")
+   print("Chung Ju-yung")
+   print("Seoul, South Korea")
+   print("Tucson")
+
+elif company == "Infiniti":
+   print("Japan")
+   print("1989")
+   print("Nissan")
+   print("Yokohama, Kanagawa, Japan")
+   print("Q50")
+
+elif company == "Isuzu":
+   print("Japan")
+   print("1916")
+   print("Tokyo Ishikawajima Shipbuilding & Engineering Co. and Tokyo Gas & Electric Industrial Co.")
+   print("Yokohama, Kanagawa, Japan")
+   print("D-Max")
+
+elif company == "Jaguar":
+   print("United Kingdom")
+   print("1922")
+   print("William Lyons & William Walmsley")
+   print("Coventry, England")
+   print("F-Type")
+
+elif company == "Jeep":
+   print("United States")
+   print("1941")
+   print("Developed by Willys-Overland for the U.S. Army")
+   print("Toledo, Ohio, USA")
+   print("Wrangler")
+
+elif company == "Kia":
+   print("South Korea")
+   print("1944")
+   print("Kim Cheol-ho")
+   print("Seoul, South Korea")
+   print("Sportage")
+
+elif company == "Koenigsegg":
+   print("Sweden")
+   print("1994")
+   print("Christian von Koenigsegg")
+   print("Ängelholm, Sweden")
+   print("Jesko")
+
+else:
+   print("Try finding in part 2")
+   print("Or the information may not be")
+
+print("Press enter to exit")
