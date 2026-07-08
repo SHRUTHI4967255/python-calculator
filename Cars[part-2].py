84a0417bfff4038895802a39639ac1c315c7c59f
K Shruthi <s4967255@gmail.com>
2026-07-08 19:51:57 +0530

    Data about 45 Car brands.
    This project has the information about 45 Car brands. With the information of company name,origin country, started year,founders,hadquaters,famous model.
    I used if,elif,else in this project.
    This cars data project also has 1st part.

---
+++ Cars[part-2].py
@@ -0,0 +1,315 @@
+print("             CARS[part-2]             ")
+print("Here is the data about car companies")
+print("This has company name,origin country,started year,Founders,Headquaters,Famous model")
+print("Use 1st letter capital to find the result")
+company = input("Enter the car company: ")
+if company == "Lamborghini":
+   print("Italy")
+   print("1963")
+   print("Ferruccio Lamborghini")
+   print("Sant'Agata Bolognese, Italy")
+   print("Huracán")
+
+elif company == "Lancia":
+   print("Italy")
+   print("1906")
+   print("Vincenzo Lancia & Claudio Fogolin")
+   print("Turin, Italy")
+   print("Ypsilon")
+
+elif company == "Land Rover":
+   print("United Kingdom")
+   print("1948")
+   print("Rover Company")
+   print("Coventry, England")
+   print("Defender")
+
+elif company == "Leapmotor":
+   print("China")
+   print("2015")
+   print("Zhu Jiangming")
+   print("Hangzhou, Zhejiang, China")
+   print("C10")
+
+elif company == "Lexus":
+   print("Japan")
+   print("1989")
+   print("Toyota")
+   print("Nagoya, Aichi, Japan")
+   print("RX")
+
+elif company == "Lincoln":
+   print("United States")
+   print("1917")
+   print("Henry M. Leland")
+   print("Dearborn, Michigan, USA")
+   print("Navigator")
+
+elif company == "Lotus":
+   print("United Kingdom")
+   print("1948")
+   print("Colin Chapman")
+   print("Hethel, Norfolk, England")
+   print("Emira")
+
+elif company == "Lucid Motors":
+   print("United States")
+   print("2007")
+   print("Bernard Tse & Sam Weng")
+   print("Newark, California, USA")
+   print("Air")
+
+elif company == "Mahindra & Mahindra":
+   print("India")
+   print("1945")
+   print("J. C. Mahindra, K. C. Mahindra & Malik Ghulam Muhammad")
+   print("Mumbai, Maharashtra, India")
+   print("Scorpio")
+
+elif company == "Maruti Suzuki":
+   print("India")
+   print("1981")
+   print("Government of India (later partnered with Suzuki)")
+   print("New Delhi, India")
+   print("Swift")
+
+elif company == "Maserati":
+   print("Italy")
+   print("1914")
+   print("Maserati brothers")
+   print("Modena, Italy")
+   print("MC20")
+
+elif company == "Mazda":
+   print("Japan")
+   print("1920")
+   print("Jujiro Matsuda")
+   print("Hiroshima, Japan")
+   print("MX-5 Miata")
+
+elif company == "McLaren Automotive":
+   print("United Kingdom")
+   print("2010")
+   print("McLare Group")
+   print("Woking, Surrey, England")
+   print("720S")
+
+elif company == "Mercedes-Benz":
+   print("Germany")
+   print("1926")
+   print("Karl Benz & Gottlieb Daimler")
+   print("Stuttgart, Germany")
+   print("S-Class")
+
+elif company == "MG Motor":
+   print("United Kingdom (brand origin)")
+   print("1924")
+   print("Cecil Kimber")
+   print("Shanghai, China (current global HQ under SAIC)")
+   print("MG4 EV")
+
+elif company == "Mini":
+   print("United Kingdom")
+   print("1969 (brand)")
+   print("Created by British Motor Holdings")
+   print("Oxford, England")
+   print("Cooper")
+
+elif company == "Mitsubishi Motors":
+   print("Japan")
+   print("1970")
+   print("Mitsubishi Group")
+   print("Tokyo, Japan")
+   print("Outlander")
+
+elif company == "NIO":
+   print("China")
+   print("2014")
+   print("William Li")
+   print("Shanghai, China")
+   print("ET5")
+
+elif company == "Nissan":
+   print("Japan")
+   print("1933")
+   print("Yoshisuke Aikawa")
+   print("Yokohama, Kanagawa, Japan")
+   print("GT-R")
+
+elif company == "Opel":
+   print("Germany")
+   print("1862")
+   print("Adam Opel")
+   print("Rüsselsheim am Main, Germany")
+   print("Corsa")
+
+elif company == "Pagani":
+   print("Italy")
+   print("1992")
+   print("Horacio Pagani")
+   print("San Cesario sul Panaro, Italy")
+   print("Huayra")
+
+elif company == "Peugeot":
+   print("France")
+   print("1810")
+   print("Peugeot family")
+   print("Poissy, France")
+   print("208")
+
+elif company == "Polestar":
+   print("Sweden")
+   print("1996")
+   print("Flash Engineering (later acquired by Volvo Cars)")
+   print("Gothenburg, Sweden")
+   print("Polestar 2")
+
+elif company == "Pontiac":
+   print("United States")
+   print("1926")
+   print("General Motors")
+   print("Detroit, Michigan, USA")
+   print("GTO")
+
+elif company == "Porsche":
+   print("Germany")
+   print("1931")
+   print("Ferdinand Porsche")
+   print("Stuttgart, Germany")
+   print("911")
+
+elif company == "Qoros":
+   print("China")
+   print("2007")
+   print("Joint venture of Chery and Israel Corporation")
+   print("Shanghai, China")
+   print("Qoros 3")
+
+elif company == "RAM":
+   print("United States")
+   print("2010")
+   print("Created by Chrysler")
+   print("Auburn Hills, Michigan, USA")
+   print("1500")
+
+elif company == "Renault":
+   print("France")
+   print("1899")
+   print("Louis Renault, Marcel Renault & Fernand Renault")
+   print("Boulogne-Billancourt, France")
+   print("Clio")
+
+elif company == "Rimac Automobili":
+   print("Croatia")
+   print("2009")
+   print("Mate Rimac")
+   print("Sveta Nedelja, Croatia")
+   print("Nevera")
+
+elif company == "Rolls-Royce Motor Cars":
+   print("United Kingdom")
+   print("1998 (current company)")
+   print("BMW")
+   print("Goodwood, West Sussex, England")
+   print("Phantom")
+
+elif company == "SAIC Motor":
+   print("China")
+   print("1955")
+   print("Shanghai Municipal Government")
+   print("Shanghai, China")
+   print("MG ZS")
+
+elif company == "SEAT":
+   print("Spain")
+   print("1950")
+   print("Instituto Nacional de Industria")
+   print("Martorell, Spain")
+   print("Ibiza")
+
+elif company == "Škoda Auto":
+   print("Czech Republic")
+   print("1895")
+   print("Václav Laurin & Václav Klement")
+   print("Mladá Boleslav, Czech Republic")
+   print("Octavia")
+
+elif company == "Smart":
+   print("Germany")
+   print("1994")
+   print("Joint venture of Mercedes-Benz and Swatch")
+   print("Böblingen, Germany")
+
+elif company == "Suzuki":
+   print("Japan")
+   print("1909")
+   print("Michio Suzuki")
+   print("Hamamatsu, Shizuoka, Japan")
+   print("Swift")
+
+elif company == "Tata Motors":
+   print("India")
+   print("1945")
+   print("Founded by the Tata Group (as TELCO)")
+   print("Mumbai, Maharashtra, India")
+   print("Nexon")
+
+elif company == "Tesla":
+   print("United States")
+   print("2003")
+   print("Martin Eberhard & Marc Tarpenning")
+   print("Austin, Texas, USA")
+   print("Model 3")
+
+elif company == "Toyota":
+   print("Japan")
+   print("1937")
+   print("Kiichiro Toyoda")
+   print("Toyota City, Aichi, Japan")
+   print("Corolla")
+
+elif company == "Vauxhall":
+   print("United Kingdom")
+   print("1857")
+   print("Alexander Wilson")
+   print("Luton, Bedfordshire, England")
+   print("Corsa")
+
+elif company == "VinFast":
+   print("Vietnam")
+   print("2017")
+   print("Phạm Nhật Vượng")
+   print("Hanoi, Vietnam")
+   print("VF 8")
+
+elif company == "Volkswagen":
+   print("Germany")
+   print("1937")
+   print("German Labour Front (Deutsche Arbeitsfront)")
+   print("Wolfsburg, Germany")
+   print("Golf")
+
+elif company == "Volvo Cars":
+   print("Sweden")
+   print("1927")
+   print("Assar Gabrielsson & Gustaf Larson")
+   print("Gothenburg, Sweden")
+   print("XC60")
+
+elif company == "Wuling":
+   print("China")
+   print("2002 (current joint venture)")
+   print("SAIC–GM–Wuling Joint Venture")
+   print("Liuzhou, Guangxi, China")
+   print("Hongguang Mini EV")
+
+elif company == "Zeekr":
+   print("China")
+   print("2021")
+   print("Geely Holding")
+   print("Ningbo, Zhejiang, China")
+   print("001")
+
+else:
+    print("Try to find your brand in part-1")
+    print("Or may the data not their,SORRY")
