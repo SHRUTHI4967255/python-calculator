f2bde0f233bf515e4bfd1e91157d613ed0022e6e
K Shruthi <s4967255@gmail.com>
2026-07-09 14:55:52 +0530

    65 countries and their rivers information
    This Python project provides information about countries and their major rivers. Users can enter a country name to view its major river and the total length of that river. The project is organized alphabetically and divided into three parts, with each part containing information for 65 countries.

---
+++ River[part-2].py
@@ -0,0 +1,196 @@
+from prompt_toolkit.key_binding.bindings.named_commands import uppercase_word
+
+print("       Rivers[psrt-1]        ")
+print("This project has information about few countries with their major river and length of the river")
+country = input("Enter the country name:  ")
+
+if country == "Ghana":
+    print("Largest river - Volta")
+    print("length of the river - 1,600")
+elif country == "Greece":
+    print("Largest river - Aliakmon")
+    print("length of the river - 297")
+elif country == "Grenada":
+    print("Largest river - St. John River")
+    print("length of the river - 24")
+elif country == "Guatemala":
+    print("Largest river - Motagua")
+    print("length of the river - 486")
+elif country == "Guinea":
+    print("Largest river - Niger")
+    print("length of the river - 4,180")
+elif country == "Guinea-Bissau":
+    print("Largest river - Geba")
+    print("length of the river - 550")
+elif country == "Guyana":
+    print("Largest river - Essequibo")
+    print("length of the river - 1,014")
+elif country == "Haiti":
+    print("Largest river - Artibonite")
+    print("length of the river - 321")
+elif country == "Holy See (Vatican City)":
+    print("Largest river - None (no river)")
+elif country == "Honduras":
+    print("Largest river - Patuca")
+    print("length of the river - 500")
+elif country == "Hungary":
+    print("Largest river - Danube")
+    print("length of the river - 2,888")
+elif country == "Iceland":
+    print("Largest river - Þjórsá (Thjorsa)")
+    print("length of the river - 230")
+elif country == "India":
+    print("Largest river - Ganges (Ganga)")
+    print("length of the river - 2,525")
+elif country == "Indonesia":
+    print("Largest river - Kapuas")
+    print("length of the river - 1,143")
+elif country == "Iran":
+    print("Largest river - Karun")
+    print("length of the river - 950")
+elif country == "Iraq":
+    print("Largest river - Tigris")
+    print("length of the river - 1,850")
+elif country == "Ireland":
+    print("Largest river - Shannon")
+    print("length of the river - 360")
+elif country == "Israel":
+    print("Largest river - Jordan")
+    print("length of the river - 251")
+elif country == "Italy":
+    print("Largest river - Po")
+    print("length of the river - 652")
+elif country == "Jamaica":
+    print("Largest river - Black River")
+    print("length of the river - 53")
+elif country == "Japan":
+    print("Largest river - Shinano")
+    print("length of the river - 367")
+elif country == "Jordan":
+    print("Largest river - Jordan River")
+    print("length of the river - 251")
+elif country == "Kazakhstan":
+    print("Largest river - Irtysh")
+    print("length of the river - 4,248")
+elif country == "Kenya":
+    print("Largest river - Tana")
+    print("length of the river - 1,014")
+elif country == "Kiribati":
+    print("Largest river - None (no permanent river)")
+elif country == "Kuwait":
+    print("Largest river - None (no permanent river)")
+elif country == "Kyrgyzstan":
+    print("Largest river - Naryn")
+    print("length of the river - 807")
+elif country == "Laos":
+    print("Largest river - Mekong")
+    print("length of the river - 4,350")
+elif country == "Latvia":
+    print("Largest river - Daugava")
+    print("length of the river - 1,020")
+elif country == "Lebanon":
+    print("Largest river - Litani")
+    print("length of the river - 170")
+elif country == "Lesotho":
+    print("Largest river - Orange (Senqu)")
+    print("length of the river - 2,200")
+elif country == "Liberia":
+    print("Largest river - St. Paul")
+    print("length of the river - 450")
+elif country == "Libya":
+    print("Largest river - None (no permanent river)")
+elif country == "Liechtenstein":
+    print("Largest river - Rhine")
+    print("length of the river - 1,230")
+elif country == "Lithuania":
+    print("Largest river - Nemunas (Neman)")
+    print("length of the river - 937")
+elif country == "Luxembourg":
+    print("Largest river - Moselle")
+    print("length of the river - 545")
+elif country == "Madagascar":
+    print("Largest river - Betsiboka")
+    print("length of the river - 525")
+elif country == "Malawi":
+    print("Largest river - Shire")
+    print("length of the river - 402")
+elif country == "Malaysia":
+    print("Largest river - Rajang")
+    print("length of the river - 563")
+elif country == "Maldives":
+    print("Largest river - None (no river)")
+elif country == "Mali":
+    print("Largest river - Niger")
+    print("length of the river - 4,180")
+elif country == "Malta":
+    print("Largest river - None (no permanent river)")
+elif country == "Marshall Islands":
+    print("Largest river - None (no river)")
+elif country == "Mauritania":
+    print("Largest river - Senegal")
+    print("length of the river - 1,641")
+elif country == "Mauritius":
+    print("Largest river - Grand River South East")
+    print("length of the river - 34")
+elif country == "Mexico":
+    print("Largest river - Río Grande (Rio Bravo)")
+    print("length of the river - 3,051")
+elif country == "Micronesia":
+    print("Largest river - None (no major river)")
+elif country == "Moldova":
+    print("Largest river - Dniester")
+    print("length of the river - 1,362")
+elif country == "Monaco":
+    print("Largest river - None (no river)")
+elif country == "Mongolia":
+    print("Largest river - Orkhon")
+    print("length of the river - 1,124")
+elif country == "Montenegro":
+    print("Largest river - Tara")
+    print("length of the river - 146")
+elif country == "Morocco":
+    print("Largest river - Oum Er-Rbia")
+    print("length of the river - 555")
+elif country == "Mozambique":
+    print("Largest river - Zambezi")
+    print("length of the river - 	2,574")
+elif country == "Myanmar":
+    print("Largest river - Ayeyarwady (Irrawaddy)")
+    print("length of the river - 	2,170")
+elif country == "Namibia":
+    print("Largest river - Orange")
+    print("length of the river - 2,200")
+elif country == "Nauru":
+    print("Largest river - None (no river)")
+elif country == "Nepal":
+    print("Largest river - Koshi")
+    print("length of the river - 729")
+elif country == "Netherlands":
+    print("Largest river - Rhine")
+    print("length of the river - 1,230")
+elif country == "New Zealand":
+    print("Largest river - Waikato")
+    print("length of the river - 425")
+elif country == "Nicaragua":
+    print("Largest river - 	Coco (Segovia)")
+    print("length of the river - 680")
+elif country == "Niger":
+    print("Largest river - 	Niger")
+    print("length of the river - 4,180")
+elif country == "Nigeria":
+    print("Largest river - Niger")
+    print("length of the river - 4,180")
+elif country == "North Korea":
+    print("Largest river - Yalu (Amnok)")
+    print("length of the river - 790")
+elif country == "North Macedonia":
+    print("Largest river - Vardar")
+    print("length of the river - 388")
+elif country == "Norway":
+    print("Largest river - Glomma")
+    print("length of the river - 621")
+
+else:
+    print("Try to find in part 3")
+
+print("Press enter to exit")
