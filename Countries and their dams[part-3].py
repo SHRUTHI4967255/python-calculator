c42db4c624d6ef77170ffd334cb12f4b9ee6bef6
K Shruthi <s4967255@gmail.com>
2026-07-13 19:53:14 +0530

    Countries and their dams [part - 3]
    This Python project contains information about countries around the world and their famous dams. It displays useful details such as the country name, a well-known dam, the total number of major dams, and the location of the famous dam.
    
    The project was created to improve Python programming skills while learning about world geography and important water resources. It demonstrates the use of variables, lists, loops, conditional statements, and formatted output.
    
    Features
    🌍 Country Name
    🏞️ Famous Dam
    📍 Dam Location
    💧 Approximate Number of Major Dams
    📚 Educational and easy to understand
    
    Skills Used
    
    Python
    Variables
    Lists
    Loops
    Functions (optional)
    Formatted Printing

---
+++ Countries and their dams[part-3].py
@@ -0,0 +1,135 @@
+print("      Countries and their Dam's information [part - 3]      ")
+print("This project has 3 parts")
+print("This project has information of country,famous dam,number of dams and famous dam location")
+country = input("Enter the country name: ")
+
+if country == "Palau":
+    print("Ngerimel Dam, 1 | Babeldaob Island")
+elif country == "Palestine":
+    print("Wadi Al-Qilt Dam           |                     2+ | West Bank")
+elif country == "Panama":
+    print("Bayano Dam                 |                    15+ | Panamá Province")
+elif country == "Papua New Guinea":
+    print("Yonki Dam                  |                    20+ | Eastern Highlands")
+elif country == "Paraguay":
+    print("Itaipu Dam                 |                   120+ | Alto Paraná")
+elif country == "Peru":
+    print("Gallito Ciego Dam          |                   300+ | Cajamarca")
+elif country == "Philippines":
+    print("Magat Dam                  |                    90+ | Isabela Province")
+elif country == "Poland":
+    print("Włocławek Dam              |                   140+ | Kuyavian-Pomeranian")
+elif country == "Portugal":
+    print("Alqueva Dam                |                   250+ | Évora District")
+elif country == "Qatar":
+    print("Rawdat Rashed Dam          |                     5+ | Al Rayyan")
+elif country == "Romania":
+    print("Vidraru Dam                |                   250+ | Argeș Count")
+elif country == "Russia":
+    print("Sayano-Shushenskaya Dam    |                30,000+ | Khakassia")
+elif country == "Rwanda":
+    print("Ntaruka Dam                |                    10+ | Northern Province")
+elif country == "Saint Kitts and Nevis":
+    print("No major large dam")
+elif country == "Saint Lucia":
+    print("John Compton Dam           |                      2 | Millet")
+elif country == "Saint Vincent and the Grenadines":
+    print("Dalaway Dam                |                      2 | Saint Andrew Parish")
+elif country == "Samoa":
+    print("Alaoa Dam                  |                      2 | Upolu Island")
+elif country == "San Marino":
+    print("No major large dam")
+elif country == "São Tomé and Príncipe":
+    print("Contador Dam               |                      2 | São Tomé Island")
+elif country == "Saudi Arabia":
+    print("King Fahd Dam              |                   560+ | Asir Province")
+elif country == "Senegal":
+    print("Diama Dam                  |                    15+ | Saint-Louis Region")
+elif country == "Serbia":
+    print("Đerdap I (Iron Gate I) Dam |                   120+ | Danube River")
+elif country == "Seychelles":
+    print("La Gogue Dam               |                      2 | Mahé Island")
+elif country == "Sierra Leone":
+    print("Bumbuna Dam                |                     8+ | Northern Province")
+elif country == "Singapore":
+    print("Marina Barrage             |                    17+ | Marina Bay")
+elif country == "Slovakia":
+    print("Gabčíkovo Dam              |                    90+ | Bratislava Region")
+elif country == "Slovenia":
+    print("Avče Dam                   |                    40+ | Soča River")
+elif country == "Solomon Islands":
+    print("No major large dam")
+elif country == "Somalia":
+    print("Bardhere Dam (planned)     |                      0 | Gedo Region")
+elif country == "South Africa":
+    print("Gariep Dam                 |                 5,000+ | Free State/Eastern Cape")
+elif country == "South Korea":
+    print("Soyang Dam                 |                18,000+ | Gangwon Province")
+elif country == "South Sudan":
+    print("No major large dam")
+elif country == "Spain":
+    print("Almendra Dam               |                 1,200+ | Salamanca Province")
+elif country == "Sri Lanka":
+    print("Victoria Dam               |                   700+ | Kandy District")
+elif country == "Sudan":
+    print("Merowe Dam                 |                    20+ | Northern State")
+elif country == "Suriname":
+    print("Afobaka Dam                |                    10+ | Brokopondo District")
+elif country == "Sweden":
+    print("Harsprånget Dam            |                 1,900+ | Norrbotten County")
+elif country == "Switzerland":
+    print("Grande Dixence Dam         |                   220+ | Valais Canton")
+elif country == "Syria":
+    print("Tabqa Dam                  |                    20+ | Raqqa Governorate")
+elif country == "Tajikistan":
+    print("Nurek Dam                  |                    30+ | Khatlon Region")
+elif country == "Tanzania":
+    print("Julius Nyerere Dam         |                    70+ | Morogoro Region")
+elif country == "Thailand":
+    print("Bhumibol Dam               |                   250+ | Tak Province")
+elif country == "Timor-Leste":
+    print("No major large dam")
+elif country == "Togo":
+    print("Nangbéto Dam               |                     5+ | Plateaux Region")
+elif country == "Tonga":
+    print("No major large dam")
+elif country == "Trinidad and Tobago":
+    print("Hollis Dam                 |                     5+ | Trinidad")
+elif country == "Tunisia":
+    print("Sidi Salem Dam             |                    35+ | Béja Governorate")
+elif country == "Turkey":
+    print("Atatürk Dam                |                   930+ | Şanlıurfa Province")
+elif country == "Turkmenistan":
+    print("Dostluk Dam                |                    20+ | Mary Region")
+elif country == "Tuvalu":
+    print("No major large dam")
+elif country == "Uganda":
+    print("Nalubaale Dam              |                    25+ | Jinja")
+elif country == "Ukraine":
+    print("DniproHES Dam              |                 1,100+ | Zaporizhzhia")
+elif country == "United Arab Emirates":
+    print("Wadi Al Beih Dam           |                   130+ | Ras Al Khaimah")
+elif country == "United Kingdom":
+    print("Kielder Dam                |                   600+ | Northumberland")
+elif country == "United States":
+    print("Hoover Dam                 |                91,000+ | Nevada–Arizona")
+elif country == "Uruguay":
+    print("Salto Grande Dam           |                   120+ | Salto Department")
+elif country == "Uzbekistan":
+    print("Charvak Dam                |                    60+ | Tashkent Region")
+elif country == "Vanuatu":
+    print("No major large dam")
+elif country == "Vatican City":
+    print("No dams")
+elif country == "Venezuela":
+    print("Guri Dam                   |                   120+ | Bolívar State")
+elif country == "Vietnam":
+    print("Sơn La Dam                 |                 7,000+ | Sơn La Province")
+elif country == "Yemen":
+    print("Ma'rib Dam                 |                    15+ | Ma'rib Governorate")
+elif country == "Zambia":
+    print("Kariba Dam                 |                   110+ | Southern Province")
+elif country == "Zimbabwe":
+    print("Kariba Dam                 |                   110+ | Mashonaland West")
+
+print("Press enter to exit")
