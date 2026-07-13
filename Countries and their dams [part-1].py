6196bacfa7812acca092341bc45760155a0d2664
K Shruthi <s4967255@gmail.com>
2026-07-13 19:51:48 +0530

    Countries and their dams [part - 1]
    This Python project contains information about countries around the world and their famous dams. It displays useful details such as the country name, a well-known dam, the total number of major dams, and the location of the famous dam.
    
    The project was created to improve Python programming skills while learning about world geography and important water resources. It demonstrates the use of variables, lists, loops, conditional statements, and formatted output.
    
    Features
    🌍 Country Name
    🏞️ Famous Dam
    📍 Dam Location
    💧 Approximate Number of Major Dams
    📚 Educational and easy to understand
    
    Skills Used
    
    Python
    Variables
    Lists
    Loops
    Functions (optional)
    Formatted Printing

---
+++ Countries and their dams [part-1].py
@@ -0,0 +1,140 @@
+print("      Countries and their Dam's information [part - 1]      ")
+print("This project has 3 parts")
+print("This project has information of country,famous dam,number of dams and famous dam location")
+country = input("Enter the country name: ")
+
+if country == "Afghanistan":
+   print(" Kajaki Dam, 30+, Helmand Province")
+elif country == "Albania":
+   print("Fierza Dam, 640+, Kukës County")
+elif country == "Algeria":
+   print(" Beni Haroun Dam, 70+,Mila Province")
+elif country == "Andorra":
+   print(" Engolasters Dam, 10+,Escaldes-Engordany")
+elif country == "Angola":
+   print(" Capanda Dam, 80+,Malanje Province")
+elif country == "Antigua and Barbuda":
+   print(" Potworks Dam, 1,Saint Paul Parish")
+elif country == "Argentina":
+   print(" Yacyretá Dam, 130+,Corrientes Province")
+elif country == "Armenia":
+   print(" Akhuryan Dam, 80+,Shirak Province")
+elif country == "Australia":
+   print(" Snowy Dam (Snowy Mountains Scheme), 500+,New South Wales")
+elif country == "Austria":
+   print(" Kölnbrein Dam, 5,000+,Carinthia")
+elif country == "Azerbaijan":
+   print(" Mingachevir Dam, 140+,Mingachevir")
+elif country == "Bahamas":
+   print(" No major large dams")
+elif country == "Bahrain":
+   print(" Sitra Recharge Dam, 5+,Sitra")
+elif country == "Bangladesh":
+   print(" Kaptai Dam, 1 major,Rangamati District")
+elif country == "Barbados":
+   print(" No major large dams")
+elif country == "Belarus":
+   print(" Vileyka Dam, 170+,Minsk Region")
+elif country == "Belgium":
+   print(" Gileppe Dam, 220+,Liège Province")
+elif country == "Belize":
+   print(" Chalillo Dam, 3,Cayo District")
+elif country == "Benin":
+   print(" Nangbeto Dam, 5+,Mono River")
+elif country == "Bhutan":
+   print(" Tala Dam, 20+,chukha District")
+elif country == "Bolivia":
+   print(" Misicuni Dam, 50+,Cochabamba")
+elif country == "Bosnia and Herzegovina":
+   print(" Grabovica Dam, 110+,Neretva River")
+elif country == "Botswana":
+   print(" Gaborone Dam,15+,Gaborone")
+elif country == "Brazil":
+   print(" Itaipu Dam,24,000+,Paraná")
+elif country == "Brunei":
+   print(" Benutan Dam,2,Tutong District")
+elif country == "Bulgaria":
+   print(" Iskar Dam,2,000+,sofia Province")
+elif country == "Burkina Faso":
+   print(" Bagré Dam,2,000+,Boulgou Province")
+elif country == "Burundi":
+   print(" Rwegura Dam,10+,Kayanza Province")
+elif country == "Cabo Verde":
+   print(" Poilão Dam,5+,Santiago Island")
+elif country == "Cambodia":
+   print(" Lower Sesan 2 Dam,15+,Stung Treng Province")
+elif country == "Cameroon":
+   print(" Lagdo Dam,30+,North Region")
+elif country == "Canada":
+   print(" Daniel-Johnson Dam,15,000+,Quebec")
+elif country == "Central African Republic":
+   print(" Boali Dam,5+,Ombella-M'Poko")
+elif country == "Chad":
+   print(" No major large dam")
+elif country == "Chile":
+   print(" Ralco Dam,140+,Biobío Region")
+elif country == "China":
+   print(" Three Gorges Dam, 98,000+,Hubei Province")
+elif country == "Colombia":
+   print(" Guavio Dam,700+,Cundinamarca")
+elif country == "Comoros":
+   print(" No major large dam")
+elif country == "Republic of the Congo":
+   print(" Imboulou Dam,10+,Plateaux Department")
+elif country == "Costa Rica":
+   print(" Arenal Dam,170+,Alajuela Province")
+elif country == "Croatia":
+   print(" Peruća Dam,60+,Split-Dalmatia County")
+elif country == "Cuba":
+   print(" Zaza Dam,240+,Sancti Spíritus Province")
+elif country == "Cyprus":
+   print(" Kouris Dam,100+,Limassol District")
+elif country == "Czech Republic":
+   print(" Orlík Dam,150+,south Bohemian Region")
+elif country == "Denmark":
+   print(" Tange Dam,20+,Central Denmark Region")
+elif country == "Djibouti":
+   print(" Ambouli Dam,2+,Djibouti Region")
+elif country == "Dominica":
+   print("No major large dam")
+elif country == "Dominican Republic":
+   print(" Tavera Dam,35+,Santiago Province")
+elif country == "Ecuador":
+   print(" Coca Codo Sinclair Dam,130+,Napo Province")
+elif country == "Egypt":
+   print(" Aswan High Dam,170+,Aswan")
+elif country == "El Salvador":
+   print(" Cerrón Grande Dam,25+,Chalatenango Department")
+elif country == "Equatorial Guinea":
+   print(" Djibloho Dam,5+,Wele-Nzas Province")
+elif country == "Eritrea":
+   print(" Kerkebet Dam,40+,Anseba Region")
+elif country == "Estonia":
+   print(" Narva Reservoir Dam,20+,Ida-Viru County")
+elif country == "Eswatini":
+   print(" Maguga Dam,10+,Hhohho Region")
+elif country == "Ethiopia":
+   print(" Grand Ethiopian Renaissance Dam (GERD),80+,Benishangul-Gumuz Region")
+elif country == "Fiji":
+   print(" Monasavu Dam,10+,Viti Levu")
+elif country == "Finland":
+   print(" Lokka Dam,5,000+,Lapland")
+elif country == "France":
+   print(" Serre-Ponçon Dam,700+,Hautes-Alpes")
+elif country == "Gabon":
+   print(" Grand Poubara Dam,15+,Haut-Ogooué Province")
+elif country == "Gambia":
+   print(" No major large dam")
+elif country == "Georgia":
+   print(" Enguri Dam,80+,Samegrelo-Zemo Svaneti")
+elif country == "Germany":
+   print(" Rappbode Dam,300+,Saxony-Anhalt")
+elif country == "Ghana":
+   print(" Akosombo Dam,25+,Eastern Region")
+elif country == "Greece":
+   print(" Kremasta Dam,170+,Aetolia-Acarnania")
+
+else:
+   print("Try to find in next parts")
+
+print("Press enter to exit")
