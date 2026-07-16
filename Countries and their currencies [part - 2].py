8ba32192c51c372daf6183b4fabd5279158b18df
K Shruthi <s4967255@gmail.com>
2026-07-16 13:38:57 +0530

    Countries and their currencies [part-2]
    Countries and Their Currencies is a beginner-friendly Python project that provides information about different countries and their official currencies. Users can explore country names along with the currencies they use, making it a simple and educational program for learning world geography and finance. This project was built using Python basics such as variables, user input, and conditional statements.

---
+++ Countries and their currencies [part - 2].py
@@ -0,0 +1,336 @@
+print("=" * 45)
+print("               Countries and their currencies [part - 1]          ")
+print("=" * 45)
+country = print(input("Enter the country name: "))
+print(" This projrct has information about Country Name,Currency Name,Currency Symbol,ISO Currency Code,Central Bank")
+if country == "Ghana":
+    print("Currency Name - Ghanaian Cedi	")
+    print("Currency Symbol - GH₵	")
+    print("ISO Currency Code - GHS")
+    print("Central Bank - Bank of Ghana")
+elif country == "Greece":
+    print("Currency Name - 	Euro	")
+    print("Currency Symbol - €	")
+    print("ISO Currency Code - EUR	")
+    print("Central Bank - Bank of Greece")
+elif country == "Grenada":
+    print("Currency Name - East Caribbean Dollar	")
+    print("Currency Symbol - EC$	")
+    print("ISO Currency Code - XCD	")
+    print("Central Bank - Eastern Caribbean Central Bank")
+elif country == "Guatemala":
+    print("Currency Name - 	Guatemalan Quetzal	")
+    print("Currency Symbol - Q	")
+    print("ISO Currency Code - GTQ	")
+    print("Central Bank - Bank of Guatemala")
+elif country == "Guinea":
+    print("Currency Name - 	Guinean Franc	")
+    print("Currency Symbol - FG	")
+    print("ISO Currency Code - GNF	")
+    print("Central Bank - Central Bank of the Republic of Guinea")
+elif country == "Guinea-Bissau":
+    print("Currency Name - West African CFA Franc	")
+    print("Currency Symbol - CFA	")
+    print("ISO Currency Code - XOF	")
+    print("Central Bank - Central Bank of West African States (BCEAO)")
+elif country == "Guyana":
+    print("Currency Name - 	Guyanese Dollar")
+    print("Currency Symbol - G$")
+    print("ISO Currency Code - GYD")
+    print("Central Bank - Bank of Guyana")
+elif country == "Haiti":
+    print("Currency Name - 	Haitian Gourde")
+    print("Currency Symbol - 	G	")
+    print("ISO Currency Code - HTG	")
+    print("Central Bank - Bank of the Republic of Haiti")
+elif country == "Honduras":
+    print("Currency Name - 	Honduran Lempira")
+    print("Currency Symbol - L")
+    print("ISO Currency Code - HNL")
+    print("Central Bank - Central Bank of Honduras")
+elif country == "Hungary":
+    print("Currency Name - Hungarian Forint	")
+    print("Currency Symbol - Ft")
+    print("ISO Currency Code - HUF")
+    print("Central Bank - Magyar Nemzeti Bank (National Bank of Hungary")
+elif country == "Iceland":
+    print("Currency Name - Icelandic Króna	")
+    print("Currency Symbol - kr")
+    print("ISO Currency Code - ISK	")
+    print("Central Bank - Central Bank of Iceland")
+elif country == "India":
+    print("Currency Name - Indian Rupee	")
+    print("Currency Symbol - ₹	")
+    print("ISO Currency Code - INR	")
+    print("Central Bank - Reserve Bank of India (RBI)")
+elif country == "Indonesia":
+    print("Currency Name - 	Indonesian Rupiah")
+    print("Currency Symbol - Rp	")
+    print("ISO Currency Code - IDR	")
+    print("Central Bank - Bank Indonesia")
+elif country == "Iran":
+    print("Currency Name - Iranian Rial	")
+    print("Currency Symbol - ﷼")
+    print("ISO Currency Code - IRR	")
+    print("Central Bank - Central Bank of the Islamic Republic of Iran")
+elif country == "Iraq":
+    print("Currency Name - Iraqi Dinar	")
+    print("ع.د - Currency Symbol ")
+    print("ISO Currency Code - IQD")
+    print("Central Bank - Central Bank of Iraq")
+elif country == "Ireland":
+    print("Currency Name - Euro	")
+    print("Currency Symbol - €	")
+    print("ISO Currency Code - EUR")
+    print("Central Bank - Central Bank of Ireland")
+elif country == "Israel":
+    print("Currency Name - Israeli New Shekel")
+    print("Currency Symbol - ₪	")
+    print("ISO Currency Code - ILS	")
+    print("Central Bank - Bank of Israel")
+elif country == "Italy":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Bank of Italy")
+elif country == "Jamaica":
+    print("Currency Name - Jamaican Dollar")
+    print("Currency Symbol - J$")
+    print("ISO Currency Code -    JMD")
+    print("Central Bank - Bank of Jamaica")
+elif country == "Japan":
+    print("Currency Name - Japanese Yen")
+    print("Currency Symbol - ¥")
+    print("ISO Currency Code -    JPY")
+    print("Central Bank - Bank of Japan")
+elif country == "Jordan":
+    print("Currency Name - Jordanian Dinar")
+    print("Currency Symbol - JD")
+    print("ISO Currency Code -    JOD")
+    print("Central Bank - Central Bank of Jordan")
+elif country == "Kazakhstan":
+    print("Currency Name - Kazakhstani Tenge")
+    print("Currency Symbol - ₸")
+    print("ISO Currency Code -    KZT")
+    print("Central Bank - National Bank of Kazakhstan")
+elif country == "Kenya":
+    print("Currency Name - Kenyan Shilling")
+    print("Currency Symbol - KSh")
+    print("ISO Currency Code -    KES")
+    print("Central Bank - Central Bank of Kenya")
+elif country == "Kiribati":
+    print("Currency Name - Australian Dollar")
+    print("Currency Symbol - A$")
+    print("ISO Currency Code -    AUD")
+    print("Central Bank - Reserve Bank of Australia*")
+elif country == "Kuwait":
+    print("Currency Name - Kuwaiti Dinar")
+    print("Currency Symbol - KD")
+    print("ISO Currency Code -    KWD")
+    print("Central Bank - Central Bank of Kuwait")
+elif country == "Kyrgyzstan":
+    print("Currency Name - Kyrgyzstani Som")
+    print("Currency Symbol - сом")
+    print("ISO Currency Code -    KGS")
+    print("Central Bank - National Bank of the Kyrgyz Republic")
+elif country == "Laos":
+    print("Currency Name - Lao Kip")
+    print("Currency Symbol - ₭")
+    print("ISO Currency Code -    LAK")
+    print("Central Bank - Bank of the Lao P.D.R.")
+elif country == "Latvia":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Bank of Latvia")
+elif country == "Lebanon":
+    print("Currency Name - Lebanese Pound")
+    print("Currency Symbol - ل.ل")
+    print("ISO Currency Code -    LBP")
+    print("Central Bank - Banque du Liban")
+elif country == "Lesotho":
+    print("Currency Name - Lesotho Loti")
+    print("Currency Symbol - L")
+    print("ISO Currency Code -    LSL")
+    print("Central Bank - Central Bank of Lesotho")
+elif country == "Liberia":
+    print("Currency Name - Liberian Dollar")
+    print("Currency Symbol - L$")
+    print("ISO Currency Code -    LRD")
+    print("Central Bank - Central Bank of Liberia")
+elif country == "Libya":
+    print("Currency Name - Libyan Dinar")
+    print("Currency Symbol - ل.د")
+    print("ISO Currency Code -    LYD")
+    print("Central Bank - Central Bank of Libya")
+elif country == "Liechtenstein":
+    print("Currency Name - Swiss Franc")
+    print("Currency Symbol - CHF")
+    print("ISO Currency Code -    CHF")
+    print("Central Bank - Swiss National Bank")
+elif country == "Lithuania":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Bank of Lithuania")
+elif country == "Luxembourg":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Central Bank of Luxembourg")
+elif country == "Madagascar":
+    print("Currency Name - Malagasy Ariary")
+    print("Currency Symbol - Ar")
+    print("ISO Currency Code -    MGA")
+    print("Central Bank - Central Bank of Madagascar")
+elif country == "Malawi":
+    print("Currency Name - Malawian Kwacha")
+    print("Currency Symbol - MK")
+    print("ISO Currency Code -    MWK")
+    print("Central Bank - Reserve Bank of Malawi")
+elif country == "Malaysia":
+    print("Currency Name - Malaysian Ringgit")
+    print("Currency Symbol - RM")
+    print("ISO Currency Code -    MYR")
+    print("Central Bank - Central Bank of Malaysia (Bank Negara Malaysia)")
+elif country == "Maldives":
+    print("Currency Name - Maldivian Rufiyaa")
+    print("Currency Symbol - ރ")
+    print("ISO Currency Code -    MVR")
+    print("Central Bank - Maldives Monetary Authority")
+elif country == "Mali":
+    print("Currency Name - West African CFA Franc")
+    print("Currency Symbol - CFA")
+    print("ISO Currency Code -    XOF")
+    print("Central Bank - Central Bank of West African States (BCEAO)")
+elif country == "Malta":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Central Bank of Malta")
+elif country == "Marshall Islands":
+    print("Currency Name - United States Dollar")
+    print("Currency Symbol - US$")
+    print("ISO Currency Code -    USD")
+    print("Central Bank - No National Central Bank")
+elif country == "Mauritania":
+    print("Currency Name - Mauritanian Ouguiya")
+    print("Currency Symbol - UM")
+    print("ISO Currency Code -    MRU")
+    print("Central Bank - Central Bank of Mauritania")
+elif country == "Mauritius":
+    print("Currency Name - Mauritian Rupee")
+    print("Currency Symbol - ₨")
+    print("ISO Currency Code -    MUR")
+    print("Central Bank - Bank of Mauritius")
+elif country == "Mexico":
+    print("Currency Name - Mexican Peso")
+    print("Currency Symbol - $")
+    print("ISO Currency Code -    MXN")
+    print("Central Bank - Bank of Mexico (Banco de México)")
+elif country == "Micronesia":
+    print("Currency Name - United States Dollar")
+    print("Currency Symbol - US$")
+    print("ISO Currency Code -    USD")
+    print("Central Bank - No National Central Bank")
+elif country == "Moldova":
+    print("Currency Name - Moldovan Leu")
+    print("Currency Symbol - L")
+    print("ISO Currency Code -    MDL")
+    print("Central Bank - National Bank of Moldova")
+elif country == "Monaco":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - No National Central Bank")
+elif country == "Mongolia":
+    print("Currency Name - Mongolian Tögrög")
+    print("Currency Symbol - ₮")
+    print("ISO Currency Code -    MNT")
+    print("Central Bank - Bank of Mongolia")
+elif country == "Montenegro":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - Central Bank of Montenegro")
+elif country == "Morocco":
+    print("Currency Name - Moroccan Dirham")
+    print("Currency Symbol - DH")
+    print("ISO Currency Code -    MAD")
+    print("Central Bank - Bank Al-Maghrib")
+elif country == "Mozambique":
+    print("Currency Name - Mozambican Metical")
+    print("Currency Symbol - MT")
+    print("ISO Currency Code -    MZN")
+    print("Central Bank - Bank of Mozambique")
+elif country == "Myanmar":
+    print("Currency Name - Myanmar Kyat")
+    print("Currency Symbol - K")
+    print("ISO Currency Code -    MMK")
+    print("Central Bank - Central Bank of Myanmar")
+elif country == "Namibia":
+    print("Currency Name - Namibian Dollar")
+    print("Currency Symbol - N$")
+    print("ISO Currency Code -    NAD")
+    print("Central Bank - Bank of Namibia")
+elif country == "Nauru":
+    print("Currency Name - Australian Dollar")
+    print("Currency Symbol - A$")
+    print("ISO Currency Code -    AUD")
+    print("Central Bank - No National Central Bank")
+elif country == "Nepal":
+    print("Currency Name - Nepalese Rupee")
+    print("Currency Symbol - रु")
+    print("ISO Currency Code -    NPR")
+    print("Central Bank - Nepal Rastra Bank")
+elif country == "Netherlands":
+    print("Currency Name - Euro")
+    print("Currency Symbol - €")
+    print("ISO Currency Code -    EUR")
+    print("Central Bank - De Nederlandsche Bank")
+elif country == "New Zealand":
+    print("Currency Name - New Zealand Dollar")
+    print("Currency Symbol - NZ$")
+    print("ISO Currency Code -    NZD")
+    print("Central Bank - Reserve Bank of New Zealand")
+elif country == "Nicaragua":
+    print("Currency Name - Nicaraguan Córdoba")
+    print("Currency Symbol - C$")
+    print("ISO Currency Code -    NIO")
+    print("Central Bank - Central Bank of Nicaragua")
+elif country == "Niger":
+    print("Currency Name - West African CFA Franc")
+    print("Currency Symbol - CFA")
+    print("ISO Currency Code -    XOF")
+    print("Central Bank - Central Bank of West African States (BCEAO)")
+elif country == "Nigeria":
+    print("Currency Name - Nigerian Naira")
+    print("Currency Symbol - ₦")
+    print("ISO Currency Code -    NGN")
+    print("Central Bank - Central Bank of Nigeria")
+elif country == "North Korea":
+    print("Currency Name - North Korean Won")
+    print("Currency Symbol - ₩")
+    print("ISO Currency Code -    KPW")
+    print("Central Bank - Central Bank of the Democratic People's Republic of Korea")
+elif country == "North Macedonia":
+    print("Currency Name - Macedonian Denar")
+    print("Currency Symbol - ден")
+    print("ISO Currency Code -    MKD")
+    print("Central Bank - National Bank of the Republic of North Macedonia")
+elif country == "Norway":
+    print("Currency Name - Norwegian Krone")
+    print("Currency Symbol - kr")
+    print("ISO Currency Code -    NOK")
+    print("Central Bank - Norges Bank")
+elif country == "Oman":
+    print("Currency Name - Omani Rial")
+    print("Currency Symbol - ر.ع.")
+    print("ISO Currency Code -    OMR")
+    print("Central Bank - Central Bank of Oman")
+
+else:
+    print("Try to find in next part")
+
+print("Sorry if their are any mistakes")
+print("Press enter to exit")
