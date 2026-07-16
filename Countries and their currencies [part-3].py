2b671ac34ac5ac4b5186cd0742a453365e032723
K Shruthi <s4967255@gmail.com>
2026-07-16 20:51:04 +0530

    Countries and their currencies [part-3]
    Countries and Their Currencies is a beginner-friendly Python project that provides information about different countries and their official currencies. Users can explore country names along with the currencies they use, making it a simple and educational program for learning world geography and finance. This project was built using Python basics such as variables, user input, and conditional statements.

---
+++ Countries and their currencies [part-3].py
@@ -0,0 +1,142 @@
+print("=" * 45)
+print("               Countries and their currencies [part - 3]          ")
+print("=" * 45)
+country = print(input("Enter the country name: "))
+print(" This projrct has information about Country Name,Currency Name,Currency Symbol,ISO Currency Code,Central Bank")
+
+if country == "Pakistan":
+    print("Pakistani Rupee	₨	PKR	State Bank of Pakistan")
+elif country == "Palau":
+    print("United States Dollar	US$	USD	No National Central Bank")
+elif country == "Palestine":
+    print("Israeli New Shekel / Jordanian Dinar	₪ / JD	ILS / JOD	Palestine Monetary Authority")
+elif country == "Panama":
+    print("Panamanian Balboa / United States Dollar	B/. / US$	PAB / USD	National Bank of Panama")
+elif country == "Papua New Guinea":
+    print("Papua New Guinean Kina	K	PGK	Bank of Papua New Guinea")
+elif country == "Paraguay":
+    print("Paraguayan Guaraní	₲	PYG	Central Bank of Paraguay")
+elif country == "Peru":
+    print("Peruvian Sol	S/	PEN	Central Reserve Bank of Peru")
+elif country == "Philippines":
+    print("Philippine Peso	₱	PHP	Bangko Sentral ng Pilipinas")
+elif country == "Poland":
+    print("Polish Złoty	zł	PLN	National Bank of Poland")
+elif country == "Portugal":
+    print("Euro	€	EUR	Bank of Portugal")
+elif country == "Qatar":
+    print("Qatari Riyal	ر.ق	QAR	Qatar Central Bank")
+elif country == "Romania":
+    print("Romanian Leu	lei	RON	National Bank of Romania")
+elif country == "Russia":
+    print("Russian Ruble	₽	RUB	Central Bank of the Russian Federation")
+elif country == "Rwanda":
+    print("Rwandan Franc	FRw	RWF	National Bank of Rwanda")
+elif country == "Saint Kitts and Nevis":
+    print("East Caribbean Dollar	EC$	XCD	Eastern Caribbean Central Bank")
+elif country == "Saint Lucia":
+    print("East Caribbean Dollar	EC$	XCD	Eastern Caribbean Central Bank")
+elif country == "Saint Vincent and the Grenadines":
+    print("East Caribbean Dollar	EC$	XCD	Eastern Caribbean Central Bank")
+elif country == "Samoa":
+    print("Samoan Tala	T	WST	Central Bank of Samoa")
+elif country == "San Marino":
+    print("Euro	€	EUR	Central Bank of San Marino")
+elif country == "São Tomé and Príncipe":
+    print("São Tomé and Príncipe Dobra	Db	STN	Central Bank of São Tomé and Príncipe")
+elif country == "Saudi Arabia":
+    print("Saudi Riyal	﷼	SAR	Saudi Central Bank (SAMA)")
+elif country == "Senegal":
+    print("West African CFA Franc	CFA	XOF	Central Bank of West African States (BCEAO)")
+elif country == "Serbia":
+    print("Serbian Dinar	дин.	RSD	National Bank of Serbia")
+elif country == "Seychelles":
+    print("Seychellois Rupee	₨	SCR	Central Bank of Seychelles")
+elif country == "Sierra Leone":
+    print("Sierra Leonean Leone	Le	SLE	Bank of Sierra Leone")
+elif country == "Singapore":
+    print("Singapore Dollar	S$	SGD	Monetary Authority of Singapore")
+elif country == "Slovakia":
+    print("Euro	€	EUR	National Bank of Slovakia")
+elif country == "Slovenia":
+    print("Euro	€	EUR	Bank of Slovenia")
+elif country == "Solomon Islands":
+    print("Solomon Islands Dollar	SI$	SBD	Central Bank of Solomon Islands")
+elif country == "Somalia":
+    print("Somali Shilling	Sh.So.	SOS	Central Bank of Somalia")
+elif country == "South Africa":
+    print("South African Rand	R	ZAR	South African Reserve Bank")
+elif country == "South Korea":
+    print("South Korean Won	₩	KRW	Bank of Korea")
+elif country == "South Sudan":
+    print("South Sudanese Pound	SSP	SSP	Bank of South Sudan")
+elif country == "Spain":
+    print("Euro	€	EUR	Bank of Spain")
+elif country == "Sri Lanka":
+    print("Sri Lankan Rupee	Rs	LKR	Central Bank of Sri Lanka")
+elif country == "Sudan":
+    print("Sudanese Pound	SDG	SDG	Central Bank of Sudan")
+elif country == "Suriname":
+    print("Surinamese Dollar	Sr$	SRD	Central Bank of Suriname")
+elif country == "Sweden":
+    print("Swedish Krona	kr	SEK	Sveriges Riksbank")
+elif country == "Switzerland":
+    print("Swiss Franc	CHF	CHF	Swiss National Bank")
+elif country == "Syria":
+    print("Syrian Pound	£S	SYP	Central Bank of Syria")
+elif country == "Tajikistan":
+    print("Tajikistani Somoni	SM	TJS	National Bank of Tajikistan")
+elif country == "Tanzania":
+    print("Tanzanian Shilling	TSh	TZS	Bank of Tanzania")
+elif country == "Thailand":
+    print("Thai Baht	฿	THB	Bank of Thailand")
+elif country == "Timor-Leste":
+    print("United States Dollar	US$	USD	Central Bank of Timor-Leste")
+elif country == "Togo":
+    print("West African CFA Franc	CFA	XOF	Central Bank of West African States (BCEAO)")
+elif country == "Tonga":
+    print("Tongan Paʻanga	T$	TOP	National Reserve Bank of Tonga")
+elif country == "Trinidad and Tobago":
+    print("Trinidad and Tobago Dollar	TT$	TTD	Central Bank of Trinidad and Tobago")
+elif country == "Tunisia":
+    print("Tunisian Dinar	د.ت	TND	Central Bank of Tunisia")
+elif country == "Turkey":
+    print("Turkish Lira	₺	TRY	Central Bank of the Republic of Türkiye")
+elif country == "Turkmenistan":
+    print("Turkmenistani Manat	m	TMT	Central Bank of Turkmenistan")
+elif country == "Tuvalu":
+    print("Australian Dollar	A$	AUD	No National Central Bank")
+elif country == "Uganda":
+    print("Ugandan Shilling	USh	UGX	Bank of Uganda")
+elif country == "Ukraine":
+    print("Ukrainian Hryvnia	₴	UAH	National Bank of Ukraine")
+elif country == "United Arab Emirates":
+    print("UAE Dirham	د.إ	AED	Central Bank of the UAE")
+elif country == "United Kingdom":
+    print("Pound Sterling	£	GBP	Bank of England")
+elif country == "United States":
+    print("United States Dollar	$	USD	Federal Reserve System")
+elif country == "Uruguay":
+    print("Uruguayan Peso	$U	UYU	Central Bank of Uruguay")
+elif country == "Uzbekistan":
+    print("Uzbekistani Soʻm	soʻm	UZS	Central Bank of Uzbekistan")
+elif country == "Vanuatu":
+    print("Vanuatu Vatu	VT	VUV	Reserve Bank of Vanuatu")
+elif country == "Vatican City":
+    print("Euro	€	EUR	Institute for the Works of Religion (Vatican Financial Institution)")
+elif country == "Venezuela":
+    print("Venezuelan Bolívar	Bs.	VES	Central Bank of Venezuela")
+elif country == "Vietnam":
+    print("Vietnamese Đồng	₫	VND	State Bank of Vietnam")
+elif country == "Yemen":
+    print("Yemeni Rial	﷼	YER	Central Bank of Yemen")
+elif country == "Zambia":
+    print("Zambian Kwacha	ZK	ZMW	Bank of Zambia")
+elif country == "Zimbabwe":
+    print("Zimbabwe Gold (ZiG)	ZiG	ZWG	Reserve Bank of Zimbabwe")
+
+else:
+      print("Try to find in before part")
+
+print("Sorry if their are any mistakes")
+print("Press enter to exit")
