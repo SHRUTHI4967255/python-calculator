9376921eb33fba7e4d5365ba5a74ac27adc2f770
K Shruthi <s4967255@gmail.com>
2026-07-18 21:54:44 +0530

    Morse code of single numbers
    This is a beginner Python project that converts a number into its Morse code representation.
    
    The user enters a single digit (0–9), and the program displays the corresponding Morse code using if-elif conditions.
    
    This project helped me practice:
    • User input
    • if-elif statements
    • print() function
    • Basic program flow
    
    Created by Shruthi as part of my Python learning journey.

---
+++ Morse code of numbers.py
@@ -0,0 +1,27 @@
+print("Morse code of Numbers")
+number = print(input("Enter a single number: "))
+if number == "0":
+    print("-----")
+elif number == "1":
+    print(".----")
+elif number == "2":
+    print("..---")
+elif number == "3":
+    print("...--")
+elif number == "4":
+    print("....-")
+elif number == "5":
+    print(".....")
+elif number == "6":
+    print("-....")
+elif number == "7":
+    print("--...")
+elif number == "8":
+    print("---..")
+elif number == "9":
+    print("----.")
+elif number == "0":
+    print("-----")
+
+print("Thank you")
+
