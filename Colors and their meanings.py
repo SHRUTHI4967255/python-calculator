0f4f9aca6bbb4b1badeb1ffbbfed27b963f66718
K Shruthi <s4967255@gmail.com>
2026-07-22 19:00:25 +0530

    Colors and their meanings
    Color Meaning Finder is a beginner-friendly Python project that displays the symbolic meaning of different colors based on user input. It helps users understand what each color commonly represents, such as love, peace, courage, creativity, and happiness. This project was built using Python fundamentals like user input, if-elif statements, and string handling.

---
+++ Colors and their meanings.py
@@ -0,0 +1,48 @@
+print("Colors and their meanings")
+color = input("Enter the color: ").capitalize()
+
+if color == "Red":
+    print("Love, Passion, Energy, Courage")
+elif color == "Orange":
+    print("Creativity, Enthusiasm, Adventure")
+elif color == "Yellow":
+    print("Happiness, Positivity, Hope")
+elif color == "Green":
+    print("Nature, Growth, Peace, Health")
+elif color == "Blue":
+    print("Trust, Calmness, Loyalty")
+elif color == "Purple":
+    print("Royalty, Wisdom, Luxury")
+elif color == "Pink":
+    print("Kindness, Care, Compassion")
+elif color == "White":
+    print("Peace, Purity, Simplicity")
+elif color == "Black":
+    print("Power, Elegance, Mystery")
+elif color == "Brown":
+    print("Stability, Reliability, Earth")
+elif color == "Gray":
+    print("Balance, Neutrality, Maturity")
+elif color == "Silver":
+    print("Modern, Sophisticated, Success")
+elif color == "Gold":
+    print("Wealth, Achievement, Victory")
+elif color == "Cyan":
+    print("Freshness, Relaxation")
+elif color == "Teal":
+    print("Emotional Balance, Healing")
+elif color == "Lime":
+    print("Energy, Youthfulness")
+elif color == "Lavender":
+    print("Grace, Calmness, Femininity")
+elif color == "Peach":
+    print("Warmth, Friendliness")
+elif color == "Navy Blue":
+    print("Confidence, Intelligence")
+elif color == "Maroon":
+    print("Strength, Determination")
+
+else:
+    print("Sorry ,couldn't find the meaning")
+
+print("Press enter to exit")
